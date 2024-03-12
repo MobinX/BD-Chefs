246e7ff4f278ed83d6c4f328c88496c77af23268
Mobin Chowdhury <47663360+MobinX@users.noreply.github.com>
2024-03-12 19:32:03 +0000

    Refactor cookie_utils.js and update getCookie function

--- src/services/cookie_utils.js
+++ src/services/cookie_utils.js
@@ -1,15 +1,39 @@
-function getCookie(name) {
-  const value = `; ${document.cookie}`;
-  const parts = value.split(`; ${name}=`);
-  if (parts.length === 2) return parts.pop().split(';').shift();
+// function getCookie(name) {
+//   const value = `; ${document.cookie}`;
+//   const parts = value.split(`; ${name}=`);
+//   if (parts.length === 2) return parts.pop().split(';').shift();
+// }
+// export { getCookie };
+// function setCookie(name, value, days) {
+//   let expires = '';
+//   if (days) {
+//     const date = new Date();
+//     date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+//     expires = `; expires=${date.toUTCString()}`;
+//   }
+//   document.cookie = `${name}=${value || ''}${expires}; path=/`;
+// }
+
+import { token }  from "@/app_const";
+
+const headers = {
+  'Content-Type': 'application/json; charset=UTF-8',
+  'zoneId': JSON.stringify(getCookie('zoneId') || ''),
+  'localizationKey': getCookie('languageCode') || '',
+  'latitude': JSON.stringify(getCookie('latitude') || ''),
+  'longitude': JSON.stringify(getCookie('longitude') || ''),
+  'Authorization': `Bearer ${getCookie('token') || ''}`
+};
+
+
+const dummydata = {
+  latitude: 23.810331,
+  longitude: 90.412521,
+  languageCode: "en",
+  token: token,
+  zoneId: 1
 }
-export { getCookie };
-function setCookie(name, value, days) {
-  let expires = '';
-  if (days) {
-    const date = new Date();
-    date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
-    expires = `; expires=${date.toUTCString()}`;
-  }
-  document.cookie = `${name}=${value || ''}${expires}; path=/`;
+
+function getCookie(name) {
+  return dummydata[name];
 }
